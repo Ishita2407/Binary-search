8961c0a3960089599fcbfbeacd34bcb535ae00ac
ISHITA <91796142+Ishita2407@users.noreply.github.com>
2024-07-13 14:53:49 +0530

    Create Lower Bound.java

---
+++ Lower Bound.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class tUf {
+
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+} 
+
+Time Complexity: O(logN), where N = size of the given array.
+Reason: We are basically using the Binary Search algorithm.
+
+Space Complexity: O(1) as we are using no extra space.
